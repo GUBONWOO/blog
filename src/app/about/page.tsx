058dfa648f128b5cb6bf8fee0b6c29368436e8d3
GUBONWOO <zcwxzsx@gmail.com>
2023-10-13 11:53:15 +0900

    make_blog banner

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,26 @@
+import Hero from '@/components/Hero';
 import React from 'react';
 
+const TITLE_CLASS = 'text-2xl font-bold text-gray-800 my-2';
 export default function AboutPage() {
-  return <p>어바웃</p>;
+  return (
+    <>
+      <Hero />
+      <section className='bg-gray-100 shadow-lg p-8 m-8 text-center'>
+        <h2 className={TITLE_CLASS}>Who Am I?</h2>
+        <p>
+          개발자 <br />
+          미경력
+        </p>
+        <h2 className={TITLE_CLASS}>Career</h2>
+        <p>
+          우리집 보안관 <br />
+          백수 <br />
+          놈팽이{' '}
+        </p>
+        <h2 className={TITLE_CLASS}>Skills</h2>
+        <p>react, vue</p>
+      </section>
+    </>
+  );
 }
